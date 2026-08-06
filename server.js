29839a0b20d5267944eb724898c1864dad7aee48
patolino2024n-tech <patolino2024n@gmail.com>
2026-08-05 22:23:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,44 @@
-{
-  "name": "deriva-sobrevivencia-estelar",
-  "version": "1.0.0",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "socket.io": "^4.7.2"
-  }
-}
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('.'));
+
+const salas = {};
+
+io.on('connection', (socket) => {
+  socket.on('criar_sala', ({ nomeSala, senha }) => {
+    if (salas[nomeSala]) {
+      socket.emit('erro', 'Essa sala já existe.');
+      return;
+    }
+    salas[nomeSala] = { senha, jogadores: [socket.id] };
+    socket.join(nomeSala);
+    socket.emit('sala_criada', { nomeSala });
+    io.emit('lista_salas', Object.keys(salas));
+  });
+
+  socket.on('entrar_sala', ({ nomeSala, senha }) => {
+    const sala = salas[nomeSala];
+    if (!sala) {
+      socket.emit('erro', 'Sala não encontrada.');
+      return;
+    }
+    if (sala.senha !== senha) {
+      socket.emit('erro', 'Senha incorreta.');
+      return;
+    }
+    sala.jogadores.push(socket.id);
+    socket.join(nomeSala);
+    socket.emit('entrou_na_sala', { nomeSala });
+    io.to(nomeSala).emit('jogador_entrou', { id: socket.id });
+  });
+
+  socket.on('disconnect', () => {
+    for (const nomeSala in salas) {
+      salas[nomeSala].jogadores = salas[nomeSala].jogadores.filter(id => id !== socket.id);
+      if (s
